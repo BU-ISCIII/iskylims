13c7e6b9f3db691ce69e3caf56434cc9218d7d40
luis <chapado.l@gmx.com>
2017-07-31 18:25:30 +0200

    Delete old files

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import Document
-
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ('description', 'name','user_id','email','csv_file','convert')
-
