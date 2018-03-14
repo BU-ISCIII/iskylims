c997d331bb4c557a3975fb7b4769e63362032f38
José Luis <jlgarcia@isciii.es>
2018-03-14 16:00:00 +0100

    urls.py --> django 2.0

--- urls.py
+++ urls.py
@@ -1,15 +1,17 @@
-from django.conf.urls import include , url                                                        
+## Adaptation of code to django 2.0:
+# "from django.conf.urls import include , url" replaced by "from django.urls import path"
+# "url(r'^" replaced by "path('"
+
+from django.urls import path
 from django.conf import settings                                                                  
                                                                                                    
 from . import views                                                                               
 from django.conf.urls.static import static                                                        
 from django.views.generic import ListView, DetailView                                             
-#from polls.models import Document                                                                
-                                                                                                   
                                                                                                    
 urlpatterns = [                                                                                   
- 	url(r'^user_creation',views.user_creation, name='user_creation'),                             
-  	url(r'^user_edit',views.user_edit, name='user_edit'),                                         
+ 	path('user_creation',views.user_creation, name='user_creation'),                             
+  	path('user_edit',views.user_edit, name='user_edit'),                                         
  ]                                                                                                 
  #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)                                
                                                                                                    
