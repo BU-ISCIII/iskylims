f199e4bac93780e76b1fbe569dbf0d57292e56db
José Luis <jlgarcia@isciii.es>
2018-04-11 16:18:14 +0200

    Blog tutorials (templates + url)

--- urls.py
+++ urls.py
@@ -3,16 +3,16 @@
 # "url(r'^" replaced by "path('"
 
 from django.urls import path
-from django.conf import settings                                                                  
+from django.conf import settings
 from django.contrib.auth.views import LoginView
-from . import views                                                                               
-from django.conf.urls.static import static                                                        
-from django.views.generic import ListView, DetailView                                             
-                                                                                                  
-urlpatterns = [                                                                                   
-     path('', LoginView.as_view(template_name='iSkyLIMS_home/index.html'), name="index"),
-]                                                                                                 
-                                                                                                   
-if settings.DEBUG:                                                                                
+from . import views
+from django.conf.urls.static import static
+from django.views.generic import ListView, DetailView
+
+urlpatterns = [
+    path('', LoginView.as_view(template_name='iSkyLIMS_home/index.html'), name="index"),
+]
+
+if settings.DEBUG:
      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
