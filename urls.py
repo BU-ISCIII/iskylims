a909ee9aff5278d1a3aaf5861628b97c0f72a053
chapado.l@gmail.com <chapado.l@gmail.com>
2019-07-26 16:48:54 +0200

    change directory to iSkyLIMS_core to prepare for new django apps

--- urls.py
+++ urls.py
@@ -10,7 +10,9 @@ from django.conf.urls.static import static
 from django.views.generic import ListView, DetailView
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='iSkyLIMS_home/index.html'), name="index"),
+    path('', views.index, name = 'index'),
+    path('addNewContacts', views.add_new_contacts, name='add_new_contacts'),
+    #path('', LoginView.as_view(template_name='iSkyLIMS_home/index.html'), name="index"),
     path('contact',views.contact, name='contact'),
     path('thanks', views.thanks, name='thanks'),
     path('about-us', LoginView.as_view(template_name='iSkyLIMS_home/about_us.html'), name="about-us"),
