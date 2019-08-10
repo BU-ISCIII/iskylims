d5ea9d54f274fcbf07f1748de40c236a81336458
luissian <chapado.l@gmail.com>
2019-08-10 18:22:01 +0200

    Adapted to handle several apps in the main index page

--- utils/generic_functions.py
+++ utils/generic_functions.py
@@ -3,5 +3,10 @@ from django.conf import settings
 def get_installed_apps () :
     app_prefix = 'iSkyLIMS'
     core = 'core'
-    return list(apps for apps in settings.INSTALLED_APPS if (app_prefix in apps and not core in apps))
+    apps_list = []
+    #import pdb; pdb.set_trace()
+    apps = list(apps for apps in settings.INSTALLED_APPS if (app_prefix in apps and not core in apps))
+    for app in apps :
+        apps_list.append([app,settings.APPS_NAMES[app]])
+    return apps_list
      
