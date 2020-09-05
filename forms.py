9cf9144355d4202a9ddca18f702da7a5962fd2ad
luissian <chapado.l@gmail.com>
2020-09-05 11:35:17 +0200

    modify the deprecated ugettext by gettext when importing django.utils.translation

--- forms.py
+++ forms.py
@@ -12,78 +12,81 @@ from django.contrib.auth import get_user_model
 from django.conf import settings
 
 class ProfileCreationForm(forms.ModelForm):
- 	class Meta:
- 		model = Profile
- 		fields = ['profilePosition','profileCenter','profileArea','profileExtension',]
+        class Meta:
+                model = Profile
+                fields = ['profilePosition','profileCenter','profileArea','profileExtension',]
 
- 	def __init__(self,*args,**kwargs):
- 		super(ProfileCreationForm,self).__init__(*args,**kwargs)
- 		self.helper = FormHelper()
- 		self.helper.form_action=""
- 		self.helper.form_method="POST"
- 		self.helper.form_tag=False
- 		self.helper.csrf=False
+        def __init__(self,*args,**kwargs):
+                super(ProfileCreationForm,self).__init__(*args,**kwargs)
+                self.helper = FormHelper()
+                self.helper.form_action=""
+                self.helper.form_method="POST"
+                self.helper.form_tag=False
+                self.helper.csrf=False
+                self.fields['profilePosition'].required = True
+                self.fields['profileCenter'].required = True
+                self.fields['profileArea'].required = True
 
- 		self.helper.layout = layout.Layout(
-          		layout.Div(
-          			layout.HTML(u"""<div class="panel-heading"><h3 class="panel-title">User data</h3></div>"""),
-          			layout.Div(
-          				layout.Div(
-          					layout.Field("profilePosition"),
-          					layout.Field("profileCenter"),
-          					css_class="col-md-6",
-          				),
-          				layout.Div(
-          					layout.Field("profileArea"),
-          					layout.Field("profileExtension"),
-          					css_class="col-md-6",
-          				),
-          				css_class = 'row panel-body',
-          			),
- 		 			css_class = 'panel panel-default',
-          			),
-          		)
+                self.helper.layout = layout.Layout(
+                        layout.Div(
+                                layout.HTML(u"""<div class="panel-heading"><h3 class="panel-title">User data</h3></div>"""),
+                                layout.Div(
+                                        layout.Div(
+                                                layout.Field("profilePosition"),
+                                                layout.Field("profileCenter"),
+                                                css_class="col-md-6",
+                                        ),
+                                        layout.Div(
+                                                layout.Field("profileArea"),
+                                                layout.Field("profileExtension"),
+                                                css_class="col-md-6",
+                                        ),
+                                        css_class = 'row panel-body',
+                                ),
+                                        css_class = 'panel panel-default',
+                                ),
+                        )
 
 
 class UserCreationForm(UserCreationForm):
 
- 	class Meta(UserCreationForm.Meta):
- 		model = User
- 		fields = ['username','email','password1','password2','first_name','last_name',]
- 		help_texts ={
- 				'email': _('Enter your email with domain @isciii.es or @externos.isciii.es'),
- 				}
+        class Meta(UserCreationForm.Meta):
+                model = User
+                fields = ['username','email','password1','password2','first_name','last_name',]
+                help_texts ={
+                                'email': _('Enter your email with domain @isciii.es or @externos.isciii.es'),
+                                }
 
- 	def __init__(self,*args, **kwargs):
- 		super(UserCreationForm, self).__init__(*args, **kwargs)
- 		self.helper = FormHelper()
- 		self.helper.form_action=""
- 		self.helper.form_method="POST"
- 		self.helper.form_tag=False
- 		self.helper.csrf=False
+        def __init__(self,*args, **kwargs):
+                super(UserCreationForm, self).__init__(*args, **kwargs)
+                self.helper = FormHelper()
+                self.helper.form_action=""
+                self.helper.form_method="POST"
+                self.helper.form_tag=False
+                self.helper.csrf=False
 
- 		self.fields['email'].required=True
- 		self.fields['first_name'].required=True
- 		self.fields['last_name'].required=True
+                self.fields['email'].required=True
+                self.fields['first_name'].required=True
+                self.fields['last_name'].required=True
 
- 		self.helper.layout = layout.Layout(
-    				layout.Div(
-    					layout.HTML(u"""<div class="panel-heading"><h3 class="panel-title">Researcher data</h3></div>"""),
-    					layout.Div(
-    						layout.Div(
-    							layout.Field("username"),
-    							layout.Field("first_name"),
-    							layout.Field("password1"),
-    							css_class="col-md-6",
-    						),
-    						layout.Div(
-    							bootstrap.PrependedText("email","@",css_class="input-block-level",placeholder="contact@example.com"),
-    							layout.Field("last_name"),
-    							layout.Field("password2"),
-    							css_class="col-md-6",
-    						),
-    						css_class = 'row panel-body',
-    					),
-    					css_class = 'panel panel-default'
-    					),
-      	)
+                self.helper.layout = layout.Layout(
+                                layout.Div(
+                                        layout.HTML(u"""<div class="panel-heading"><h3 class="panel-title">Researcher data</h3></div>"""),
+                                        layout.Div(
+                                                layout.Div(
+                                                        layout.Field("username"),
+                                                        layout.Field("first_name"),
+                                                        layout.Field("password1"),
+                                                        css_class="col-md-6",
+                                                ),
+                                                layout.Div(
+                                                        bootstrap.PrependedText("email","@",css_class="input-block-level",placeholder="contact@example.com"),
+                                                        layout.Field("last_name"),
+                                                        layout.Field("password2"),
+                                                        css_class="col-md-6",
+                                                ),
+                                                css_class = 'row panel-body',
+                                        ),
+                                        css_class = 'panel panel-default'
+                                        ),
+        )
--- models.py
+++ models.py
@@ -2,8 +2,8 @@ from django.db import models
 import os
 from django.db import models
 from django import forms
-from django.utils.encoding import python_2_unicode_compatible
-from django.utils.translation import ugettext_lazy as _
+#from django.utils.encoding import python_2_unicode_compatible
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User,AbstractUser
 #from django.db.models.signals import post_save
 #from django.dispatch import receiver
