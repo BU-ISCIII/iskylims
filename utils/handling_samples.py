8e454445cb8f0c5260258e4ac2f2eddb3c1fc4e2
luissian <chapado.l@gmail.com>
2020-09-29 20:13:17 +0200

    Fix error when fetching the valid user lot kits

--- utils/handling_samples.py
+++ utils/handling_samples.py
@@ -39,6 +39,7 @@ def display_molecule_protocol_parameters (molecule_ids, user_obj):
         molecule_ids
     Functions:
         get_protocol_parameters_and_type  # located at iSkyLIMS_core.handling_protocols.py
+        get_lot_commercial_kits  # located at iSkyLIMS_core.handling_commercial_kits.py
     Return:
         laboratories.
     '''
