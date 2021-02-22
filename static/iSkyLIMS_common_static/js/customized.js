0d580a06898784c4d961beb11714452795ee6cb1
luissian <chapado.l@gmail.com>
2021-02-22 11:20:39 +0100

    add summaryBin javascript function

--- static/iSkyLIMS_common_static/js/customized.js
+++ static/iSkyLIMS_common_static/js/customized.js
@@ -1,3 +1,20 @@
+/////////// Fade Tabs ////////
+
+function summaryBin(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
 // Download a table to excel //
 function exportTableToExcel(tableID, filename = ''){
     var downloadLink;
