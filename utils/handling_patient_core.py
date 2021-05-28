98bbc5ca8c55bad531b2d8f831e4ec19b929e1dc
luissian <chapado.l@gmail.com>
2021-05-28 16:55:39 +0200

    small change in patient core_obr_from_id

--- utils/handling_patient_core.py
+++ utils/handling_patient_core.py
@@ -1,14 +1,16 @@
 from iSkyLIMS_core.models import *
-from iSkyLIMS_core.core_config import *
 
 def get_patient_core_obj_from_id (p_id):
     '''
     Description:
         The function return the patient core object from the id value
     Input:
-        p_id # "    patient id
+        p_id #    patient id
     Return:
-    patient_obj
+        patient_obj
     '''
-    patient_obj = PatientCore.objects.get(pk__exact = p_id)
+    try:
+        patient_obj = PatientCore.objects.get(pk__exact = p_id)
+    except:
+        patient_obj = None
     return patient_obj
