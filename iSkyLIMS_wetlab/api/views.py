2ad5d91d5d7d03b3e6a3b2276fdbfe23224b4a7a
luissian <chapado.l@gmail.com>
2022-04-11 15:46:56 +0200

    added functions for processind sample

--- iSkyLIMS_wetlab/api/views.py
+++ iSkyLIMS_wetlab/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from django.http import QueryDict
 
 from .serializers import CreateSampleSerializer, CreateProjectDataSerializer
-from .utils.request_handling import split_sample_data
+from .utils.request_handling import split_sample_data, include_instances_in_sample, include_codding
 
 
 @api_view(["GET"])
@@ -25,14 +25,22 @@ def sample_list(request):
 def create_sample_data(request):
     if request.method == "POST":
         data = request.data
-        import pdb; pdb.set_trace()
+
         if isinstance(data, QueryDict):
             data = data.dict()
+
         if "sample" not in data and "project" not in data:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         split_data = split_sample_data(data)
-        if not split_data:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        if not isinstance(split_data, dict):
+            return Response(split_data, status=status.HTTP_400_BAD_REQUEST)
+        inst_req_sample = include_instances_in_sample(split_data["s_data"])
+        if not isinstance(inst_req_sample, dict):
+            return Response(inst_req_sample, status=status.HTTP_400_BAD_REQUEST)
+        split_data["s_data"] = inst_req_sample
+        split_data["s_data"]["sampleUser"] = request.user
+        # Adding coding for sample
+        split_data["s_data"].update(include_codding(request.user.name), split_data["s_data"]["sampleName"])
         sample_serializer = CreateSampleSerializer(data=split_data["s_data"])
         if not sample_serializer.is_valid():
             return Response(sample_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
