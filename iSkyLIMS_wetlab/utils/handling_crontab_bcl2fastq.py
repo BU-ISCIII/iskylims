095a3d7bc90e613a6e624e863df141bafcf787c1
luissian <chapado.l@gmail.com>
2022-04-18 17:48:31 +0200

    Fixed bug described in issue https://github.com/BU-ISCIII/iSkyLIMS/issues/97

--- iSkyLIMS_wetlab/utils/handling_crontab_bcl2fastq.py
+++ iSkyLIMS_wetlab/utils/handling_crontab_bcl2fastq.py
@@ -26,21 +26,21 @@ def check_demultiplexing_folder_exists(conn, run_folder, experiment_name):
         bcl2fastq_finish_date
     '''
     logger = logging.getLogger(__name__)
-    logger.debug ('%s : Starting function check_demultiplexing_folder_exists', experiment_name)
+    logger.debug('%s : Starting function check_demultiplexing_folder_exists', experiment_name)
     bcl2fastq_finish_date = ''
-    statistics_folder = os.path.join(get_samba_application_shared_folder(), run_folder, DEMULTIPLEXION_BCL2FASTQ_FOLDER)
+    statistics_folder = os.path.join(get_samba_application_shared_folder(), run_folder, DEMULTIPLEXION_BCL2FASTQ_FOLDER, STATS_FOLDER)
 
     try:
         file_list = conn.listPath( get_samba_shared_folder(), statistics_folder)
     except:
         string_message = experiment_name + ' : Unable to fetch folder demultiplexing at  ' + statistics_folder
-        logging_errors(string_message, True, True)
-        logger.debug ('%s : End function check_demultiplexing_folder_exists with error', experiment_name)
-        return {'ERROR':29}
+        logging_warnings(string_message, True)
+        logger.debug('%s : End function check_demultiplexing_folder_exists with warning', experiment_name)
+        return {'ERROR': 29}
 
     logger.info('%s : bcl2fastq has been completed . Collecting date when finish the bcl2fastq', experiment_name)
 
-    s_conversion_stats = os.path.join (statistics_folder, STATS_FOLDER, CONVERSION_STATS_FILE)
+    s_conversion_stats = os.path.join (statistics_folder, CONVERSION_STATS_FILE)
     try:
         conversion_attributes = conn.getAttributes(get_samba_shared_folder() ,s_conversion_stats)
     except:
