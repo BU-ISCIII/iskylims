4bb92f5dea8f7d0a310774e11c04d76af65aaffb
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-04-13 16:04:02 +0200

    linting and change to snake case wetlab lab request api file

--- iSkyLIMS_wetlab/api/utils/lab_request_handling.py
+++ iSkyLIMS_wetlab/api/utils/lab_request_handling.py
@@ -3,6 +3,6 @@ from iSkyLIMS_core.models import LabRequest
 
 def get_laboratory_instance(lab_name):
     """return the laboratory instance, or None if not match"""
-    if LabRequest.objects.filter(labName__iexact=lab_name).exists():
-        return LabRequest.objects.filter(labName__iexact=lab_name).last()
+    if LabRequest.objects.filter(lab_name__iexact=lab_name).exists():
+        return LabRequest.objects.filter(lab_name__iexact=lab_name).last()
     return None
