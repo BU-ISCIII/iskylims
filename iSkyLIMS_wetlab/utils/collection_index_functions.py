4a69bb970424284f3c1d3190faef486f38c7a321
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-04-13 16:03:12 +0200

    linting and change to snake case wetlab collection index file

--- iSkyLIMS_wetlab/utils/collection_index_functions.py
+++ iSkyLIMS_wetlab/utils/collection_index_functions.py
@@ -1,12 +1,17 @@
-import re, os, sys, codecs
-import datetime, time
 import itertools
-from django.core.files.storage import FileSystemStorage
-from iSkyLIMS_wetlab.wetlab_config import *
-from iSkyLIMS_wetlab.models import *
+import os
+import re
+import time
 
-def check_collection_index_file_format (input_file):
-    '''
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+from iSkyLIMS_wetlab.models import *
+from iSkyLIMS_wetlab.wetlab_config import *
+
+
+def check_collection_index_file_format(input_file):
+    """
     Description:
         The function will check if the input file contains the heading
         described on the declared constant COLLECTION_INDEX_HEADING
@@ -23,29 +28,33 @@ def check_collection_index_file_format (input_file):
         False if heading was not found
         True if heading was found.
 
-    '''
-    with open (input_file , encoding="utf-8" ) as fh:
-       read_data = fh.read()
-    for item_to_check in wetlab_config.COLLECTION_INDEX_HEADING :
+    """
+    with open(input_file, encoding="utf-8") as fh:
+        read_data = fh.read()
+    for item_to_check in wetlab_config.COLLECTION_INDEX_HEADING:
         if item_to_check not in read_data:
             return False
     return True
 
+
 def check_collection_index_exists(collection_name):
-    '''
+    """
     Description:
         Check if collection name exist on Database
     Input:
         collection_name # name to check
     Return:
         True if exists on database , else if not
-    '''
-    if CollectionIndexKit.objects.filter( collectionIndexName__exact = collection_name).exists():
+    """
+    if CollectionIndexKit.objects.filter(
+        collection_index_name__exact=collection_name
+    ).exists():
         return True
     return False
 
-def get_collection_index_name (input_file):
-    '''
+
+def get_collection_index_name(input_file):
+    """
     Description:
         The function will read the input file and it will return
         the index library name
@@ -57,13 +66,13 @@ def get_collection_index_name (input_file):
         library_name    # will have the name of the library fetched from file
     Return:
         library_name with the name of the library or empty '' if not found
-    '''
+    """
     found_name = False
-    collection_name = ''
-    with open (input_file , encoding="utf-8" ) as fh:
+    collection_name = ""
+    with open(input_file, encoding="utf-8") as fh:
         for line in fh:
-            found_collection_name = re.search('^\[Name\]',line)
-            if found_collection_name :
+            found_collection_name = re.search("^\[Name\]", line)
+            if found_collection_name:
                 found_name = True
                 continue
             if found_name:
@@ -72,8 +81,8 @@ def get_collection_index_name (input_file):
     return collection_name
 
 
-def get_collection_settings (input_file):
-    '''
+def get_collection_settings(input_file):
+    """
     Description:
         The function will read the input file and it will return
         library_settings with the information content file
@@ -86,88 +95,95 @@ def get_collection_settings (input_file):
                 contains the information that we are looking for
     Return:
         library_settings with the settings of the library or empty '' if not found
-    '''
-    collection_index_settings ={}
+    """
+    collection_index_settings = {}
     adapter_list = []
     found_version = False
-    with open (input_file, encoding="utf-8") as fh:
+    with open(input_file, encoding="utf-8") as fh:
         for line in fh:
-            line=line.rstrip()
+            line = line.rstrip()
             if line == "":
                 continue
-            if '[Version]' in line :
-               found_version = True
-               continue
+            if "[Version]" in line:
+                found_version = True
+                continue
             if found_version:
-                collection_index_settings['version'] = line
+                collection_index_settings["version"] = line
                 found_version = False
                 continue
-            if '[Name]' in line :
+            if "[Name]" in line:
                 found_name = True
                 continue
             if found_name:
-                collection_index_settings['name'] = line
+                collection_index_settings["name"] = line
                 found_name = False
                 continue
-            if '[PlateExtension]' in line:
+            if "[PlateExtension]" in line:
                 found_extension = True
                 continue
-            if found_extension :
-                collection_index_settings['plate_extension'] = line
+            if found_extension:
+                collection_index_settings["plate_extension"] = line
                 found_extension = False
                 continue
-            if '[Settings]' in line:
+            if "[Settings]" in line:
                 found_settings = True
                 continue
-            if found_settings :
-                line_split= line.split('\t')
+            if found_settings:
+                line_split = line.split("\t")
                 # No more adapters  have been found. copy the adapters in library_settings
                 # dictionary and exit the loop
 
-                if 'Adapter' in line:
+                if "Adapter" in line:
                     adapter_list.append(line_split[-1])
-                if (len (line_split) == 1 and len(adapter_list) > 0):
-
-                    collection_index_settings['adapters'] = adapter_list
+                if len(line_split) == 1 and len(adapter_list) > 0:
+                    collection_index_settings["adapters"] = adapter_list
                     break
                 else:
                     continue
     return collection_index_settings
 
 
-def get_collection_index_information (collection_index_id) :
-
-    collection_index_dict ={}
+def get_collection_index_information(collection_index_id):
+    collection_index_dict = {}
     collection_index_obj = CollectionIndexKit.objects.get(pk=collection_index_id)
 
     general_information = [collection_index_obj.get_collection_index_information()]
-    collection_index_dict['general_information'] = general_information
+    collection_index_dict["general_information"] = general_information
 
-    if CollectionIndexValues.objects.filter(collectionIndexKit_id = collection_index_id).exists():
-        index_list = CollectionIndexValues.objects.filter(collectionIndexKit_id = collection_index_id)
+    if CollectionIndexValues.objects.filter(
+        collection_index_kit_id=collection_index_id
+    ).exists():
+        index_list = CollectionIndexValues.objects.filter(
+            collection_index_kit_id=collection_index_id
+        )
 
         I7_indexes, I5_indexes = [], []
         index_row_info = []
-        for index in index_list :
-            i_values = index.get_index_value_information().split(';')
+        for index in index_list:
+            i_values = index.get_index_value_information().split(";")
             index_row_info.append(i_values)
             # get all I7 index defined on the library
-            I7_indexes.append([i_values[1],i_values[2]])
-            if i_values[3] != '':
-                #get all I5 index defined on the library
-                I5_indexes.append([i_values[3],i_values[4]])
+            I7_indexes.append([i_values[1], i_values[2]])
+            if i_values[3] != "":
+                # get all I5 index defined on the library
+                I5_indexes.append([i_values[3], i_values[4]])
         I7_indexes.sort()
-        collection_index_dict['I7_indexes'] = list(I7_indexes for I7_indexes,_ in itertools.groupby(I7_indexes))
-        if len(I5_indexes) > 0 :
+        collection_index_dict["I7_indexes"] = list(
+            I7_indexes for I7_indexes, _ in itertools.groupby(I7_indexes)
+        )
+        if len(I5_indexes) > 0:
             I5_indexes.sort()
-            collection_index_dict['I5_indexes'] = list(I5_indexes for I5_indexes,_ in itertools.groupby(I5_indexes))
-        collection_index_dict['default_layout'] = index_row_info
+            collection_index_dict["I5_indexes"] = list(
+                I5_indexes for I5_indexes, _ in itertools.groupby(I5_indexes)
+            )
+        collection_index_dict["default_layout"] = index_row_info
         return collection_index_dict
     else:
         return False
 
-def get_index_values (input_file):
-    '''
+
+def get_index_values(input_file):
+    """
     Description:
         The function will read the index value in the input file and it will return
         library_settings with the information content file
@@ -182,82 +198,95 @@ def get_index_values (input_file):
     Return:
         index_values with the I7 and I5 index found in the file
             Empty if not found
-    '''
-    index_7 , index_5 = {} , {}
+    """
+    index_7, index_5 = {}, {}
     found_I7 = False
     found_I5 = False
     layout_found = False
     index_values = []
-    with open (input_file , encoding="utf-8") as fh :
+    with open(input_file, encoding="utf-8") as fh:
         for line in fh:
-            line=line.rstrip()
+            line = line.rstrip()
             if line == "":
                 continue
-            if '[I7]' in line :
+            if "[I7]" in line:
                 found_I7 = True
                 continue
-            if found_I7 :
-                line=line.rstrip()
-                if len (line.split('\t'))> 1:
-                    index_line = line.split('\t')
-                    index_7[index_line[0]]= index_line[1]
+            if found_I7:
+                line = line.rstrip()
+                if len(line.split("\t")) > 1:
+                    index_line = line.split("\t")
+                    index_7[index_line[0]] = index_line[1]
                     continue
                 # No more index have been found for I7. Start collecting index for I5
                 else:
                     found_I7 = False
-                    if '[I5]' in line:
+                    if "[I5]" in line:
                         found_I5 = True
                         continue
-            if found_I5 :
-                line=line.rstrip()
-                if len (line.split('\t')) > 1:
-                    index_line = line.split('\t')
-                    index_5[index_line[0]]= index_line[1]
+            if found_I5:
+                line = line.rstrip()
+                if len(line.split("\t")) > 1:
+                    index_line = line.split("\t")
+                    index_5[index_line[0]] = index_line[1]
                     continue
                 # No more index have been found for I5. Exit the loop
                 else:
                     found_I5 = False
-            if 'Layout' in line :
-                if "SingleIndex" in line and len(index_5) > 0 :
-                    if len(index_values) == 0 :
+            if "Layout" in line:
+                if "SingleIndex" in line and len(index_5) > 0:
+                    if len(index_values) == 0:
                         continue
                     else:
                         break
-                if len(index_values) > 0 :
+                if len(index_values) > 0:
                     break
                 else:
                     layout_found = True
                     continue
-            if layout_found :
-                if '[' in line: # reached the end of layout. Exiting the function
+            if layout_found:
+                if "[" in line:  # reached the end of layout. Exiting the function
                     break
-                line = line.rstrip('\n')
-                line_split = line.split('\t')
-                if len(index_5) : # There are 2 index (I7 and I5)
-                    index_values.append([line_split[0], line_split[1], index_7[line_split[1]], line_split[2], index_5[line_split[2]]])
+                line = line.rstrip("\n")
+                line_split = line.split("\t")
+                if len(index_5):  # There are 2 index (I7 and I5)
+                    index_values.append(
+                        [
+                            line_split[0],
+                            line_split[1],
+                            index_7[line_split[1]],
+                            line_split[2],
+                            index_5[line_split[2]],
+                        ]
+                    )
                 else:
-                    index_values.append([line_split[0], line_split[1], index_7[line_split[1]], '', ''])
+                    index_values.append(
+                        [line_split[0], line_split[1], index_7[line_split[1]], "", ""]
+                    )
     return index_values
 
 
-def get_list_of_collection_kits ():
-    '''
+def get_list_of_collection_kits():
+    """
     Description:
         The function get the collection kit list names defined in database
     Return:
         collection_kit_list
-    '''
+    """
     collection_kit_list = []
     if CollectionIndexKit.objects.all().exists():
-        collection_objs = CollectionIndexKit.objects.all().order_by('collectionIndexName')
+        collection_objs = CollectionIndexKit.objects.all().order_by(
+            "collectionIndexName"
+        )
         for collection in collection_objs:
             collection_kit_list.append(collection.get_collection_index_name())
         return collection_kit_list
 
     return collection_kit_list
 
+
 def store_collection_kits_file(collection_file):
-    '''
+    """
     Description:
         The function is store the collection kit file inside COLLECTION_INDEX_KITS_DIRECTORY
 
@@ -286,12 +315,12 @@ def store_collection_kits_file(collection_file):
         -- library_kit_information with :
             -- ['libraries']
             ---['new_library_kit'] in case a new library kit was added
-    '''
+    """
 
-    ## fetch the file from user form and  build the file name  including
-    ## the date and time on now to store in database
+    # fetch the file from user form and  build the file name  including
+    # the date and time on now to store in database
 
-    split_filename=re.search('(.*)(\.\w+$)',collection_file.name)
+    split_filename = re.search("(.*)(\.\w+$)", collection_file.name)
     f_name = split_filename[1]
     f_extension = split_filename[2]
     fs_index_lib = FileSystemStorage()
@@ -300,39 +329,47 @@ def store_collection_kits_file(collection_file):
     if not os.path.exists(index_directory):
         os.makedirs(index_directory)
 
-    ## using the MEDIA_ROOT variable defined on settings to upload the file
-    file_name=os.path.join(index_directory,  str(f_name + '_' +timestr + f_extension))
-    filename = fs_index_lib.save(file_name,  collection_file)
+    # using the MEDIA_ROOT variable defined on settings to upload the file
+    file_name = os.path.join(index_directory, str(f_name + "_" + timestr + f_extension))
+    fs_index_lib.save(file_name, collection_file)
     saved_file = os.path.join(settings.MEDIA_ROOT, file_name)
 
     return saved_file
 
-def store_collection_settings (collection_settings, file_name) :
+
+def store_collection_settings(collection_settings, file_name):
     # saving library settings into database
 
-    if not 'adapters' in collection_settings:
-        collection_settings['adapters'] = ['']*2
-    elif len(collection_settings['adapters']) == 1:
-        collection_settings['adapters'].append('')
-    new_collection_settings = CollectionIndexKit(collectionIndexName = collection_settings['name'],
-                                version =  collection_settings ['version'],
-                                plateExtension = collection_settings['plate_extension'] ,
-                                adapter1 = collection_settings['adapters'][0],
-                                adapter2 = collection_settings['adapters'][1],
-                                collectionIndexFile = file_name)
+    if "adapters" not in collection_settings:
+        collection_settings["adapters"] = [""] * 2
+    elif len(collection_settings["adapters"]) == 1:
+        collection_settings["adapters"].append("")
+    new_collection_settings = CollectionIndexKit(
+        collectionIndexName=collection_settings["name"],
+        version=collection_settings["version"],
+        plateExtension=collection_settings["plate_extension"],
+        adapter1=collection_settings["adapters"][0],
+        adapter2=collection_settings["adapters"][1],
+        collectionIndexFile=file_name,
+    )
     new_collection_settings.save()
     return new_collection_settings
 
-def remove_boom_bytes (input_file):
-    s = open(input_file, mode='r', encoding='utf-8-sig').read()
-    open(input_file, mode='w', encoding='utf-8').write(s)
+
+def remove_boom_bytes(input_file):
+    s = open(input_file, mode="r", encoding="utf-8-sig").read()
+    open(input_file, mode="w", encoding="utf-8").write(s)
 
 
-def store_collection_indexes (collection_index, new_collection_obj):
+def store_collection_indexes(collection_index, new_collection_obj):
     # saving index values into database
-    for row in collection_index :
-        index_to_store = CollectionIndexValues(collectionIndexKit_id = new_collection_obj,
-                                defaultWell = row[0], index_7 = row[1],
-                                i_7_seq = row[2], index_5 = row[3],
-                                i_5_seq = row[4])
+    for row in collection_index:
+        index_to_store = CollectionIndexValues(
+            collectionIndexKit_id=new_collection_obj,
+            defaultWell=row[0],
+            index_7=row[1],
+            i_7_seq=row[2],
+            index_5=row[3],
+            i_5_seq=row[4],
+        )
         index_to_store.save()
