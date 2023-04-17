e1b3694da44c9500ca0f164ef27a59f0620d72fd
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-04-17 19:04:29 +0200

    linting and snake case model variable in remaining files

--- iSkyLIMS_wetlab/utils/pool_preparation.py
+++ iSkyLIMS_wetlab/utils/pool_preparation.py
@@ -1,10 +1,12 @@
 import json
 from datetime import date
+
 from iSkyLIMS_wetlab.models import *
 from iSkyLIMS_wetlab.wetlab_config import *
 
+
 def get_lib_prep_adapter(lib_prep_ids):
-    '''
+    """
     Description:
         The function gets the different adapters used for the library preparations instance.
         Returns the adapter value
@@ -12,19 +14,19 @@ def get_lib_prep_adapter(lib_prep_ids):
         lib_prep_ids  # library preparation id list
     Return:
         returns a list of adapters in adapter_list
-    '''
+    """
     adapters = []
-    for lib_prep_id in lib_prep_ids :
-        if not LibPrepare.objects.filter(pk__exact = lib_prep_id).exists():
+    for lib_prep_id in lib_prep_ids:
+        if not LibPrepare.objects.filter(pk__exact=lib_prep_id).exists():
             continue
-        lib_prep_obj =  LibPrepare.objects.get(pk__exact = lib_prep_id)
+        lib_prep_obj = LibPrepare.objects.get(pk__exact=lib_prep_id)
         adapters.append(lib_prep_obj.get_adapters()[0])
     adapter_list = list(set(adapters))
     return adapter_list
 
 
-def check_if_duplicated_index (lib_prep_ids):
-    '''
+def check_if_duplicated_index(lib_prep_ids):
+    """
     Description:
         The function check if some of the library preparations instances has the same indexes.
         If there is not duplicated returns True else returns the list of the library preparation ids which
@@ -34,40 +36,42 @@ def check_if_duplicated_index (lib_prep_ids):
     Return:
         True if unique indexes are used.
         a dictionary with 'incompatible_index' as key and the list of samples names as value.
-    '''
-    index_values ={}
-    for lib_prep_id in lib_prep_ids :
-        lib_prep_obj =  LibPrepare.objects.get(pk__exact = lib_prep_id)
-        #lib_prep_count += 1
+    """
+    index_values = {}
+    for lib_prep_id in lib_prep_ids:
+        lib_prep_obj = LibPrepare.objects.get(pk__exact=lib_prep_id)
+        # lib_prep_count += 1
         lib_index = lib_prep_obj.get_indexes()
-        combined_index = str(lib_index['i7_indexID'] + '_' + lib_index['i5_indexID'])
-        '''
+        combined_index = str(lib_index["i7_indexID"] + "_" + lib_index["i5_indexID"])
+        """
         lib_prep_paired_end = lib_prep_obj.get_single_paired()
         if not lib_prep_paired_end in s_paired_values:
             s_paired_values[lib_prep_paired_end] = []
         s_paired_values[lib_prep_paired_end].append(lib_prep_id)
-        '''
+        """
         if combined_index not in index_values:
             index_values[combined_index] = []
         index_values[combined_index].append(lib_prep_id)
-    if len(index_values) == len(lib_prep_ids) :
-        return 'True'
+    if len(index_values) == len(lib_prep_ids):
+        return "True"
     else:
-        incompatible_samples ={}
+        incompatible_samples = {}
         incompatible_index = []
         # check which index
         for key, values in index_values.items():
             if len(values) > 1:
-                s_name  = []
+                s_name = []
                 for value in values:
-                    s_name.append(LibPrepare.objects.get(pk__exact = value).get_sample_name())
-                incompatible_index.append([' and  '.join(s_name), key])
-        incompatible_samples['incompatible_index'] =incompatible_index
+                    s_name.append(
+                        LibPrepare.objects.get(pk__exact=value).get_sample_name()
+                    )
+                incompatible_index.append([" and  ".join(s_name), key])
+        incompatible_samples["incompatible_index"] = incompatible_index
         return incompatible_samples
 
 
 def get_single_paired(lib_prep_ids):
-    '''
+    """
     Description:
         The function checks if at least one library preparation included in the pool
         was usin I5 index.
@@ -76,16 +80,16 @@ def get_single_paired(lib_prep_ids):
 
     Return:
         PairedEnd or SingleRead
-    '''
+    """
     for lib_prep_id in lib_prep_ids:
-        i5_index_value =  LibPrepare.objects.get(pk__exact = lib_prep_id).get_i5_index()
-        if i5_index_value != '' :
-            return 'PairedEnd'
-    return 'SingleRead'
+        i5_index_value = LibPrepare.objects.get(pk__exact=lib_prep_id).get_i5_index()
+        if i5_index_value != "":
+            return "PairedEnd"
+    return "SingleRead"
 
 
 def define_new_pool(form_data, user_obj):
-    '''
+    """
     Description:
         The function performs some checks to verify the new pool can be created.
 
@@ -109,11 +113,11 @@ def define_new_pool(form_data, user_obj):
 
         protocol_parameter_list.
 
-    '''
-    error ={}
-    lib_prep_ids = form_data.getlist('lib_prep_id')
+    """
+    error = {}
+    lib_prep_ids = form_data.getlist("lib_prep_id")
     if len(lib_prep_ids) == 0:
-        error['ERROR'] = ERROR_NOT_LIBRARY_PREPARATION_SELECTED
+        error["ERROR"] = ERROR_NOT_LIBRARY_PREPARATION_SELECTED
         return error
     # check if index are not duplicate in the library preparation
 
@@ -125,59 +129,60 @@ def define_new_pool(form_data, user_obj):
     #     error['incompatible_s_p_end'] = True
     #     return error
     duplicated_index = check_if_duplicated_index(lib_prep_ids)
-    if 'incompatible_index' in duplicated_index :
-        error['duplicated_index'] = duplicated_index
+    if "incompatible_index" in duplicated_index:
+        error["duplicated_index"] = duplicated_index
         return error
     # check that all library preparation use the same adapter
     adapters = get_lib_prep_adapter(lib_prep_ids)
-    if len(adapters) > 1 :
-        error['multiple_adapters'] = adapters
+    if len(adapters) > 1:
+        error["multiple_adapters"] = adapters
         return error
 
     pool_data = {}
-    pool_data['poolName'] = form_data['poolName']
-    pool_data['platform'] = form_data['platform']
-    pool_data['poolCodeID'] = generate_pool_code_id()
-    pool_data['registerUser'] = user_obj
-    pool_data['adapter'] = adapters[0]
-    pool_data['pairedEnd'] = get_single_paired(lib_prep_ids)
-    pool_data['n_samples'] = len(lib_prep_ids)
+    pool_data["poolName"] = form_data["poolName"]
+    pool_data["platform"] = form_data["platform"]
+    pool_data["poolCodeID"] = generate_pool_code_id()
+    pool_data["registerUser"] = user_obj
+    pool_data["adapter"] = adapters[0]
+    pool_data["pairedEnd"] = get_single_paired(lib_prep_ids)
+    pool_data["n_samples"] = len(lib_prep_ids)
 
     new_pool = LibraryPool.objects.create_lib_pool(pool_data)
     # update pool_id in each library_preparation belongs the new pool
     for lib_prep in lib_prep_ids:
-        lib_prep_obj = LibPrepare.objects.get(pk__exact = lib_prep)
+        lib_prep_obj = LibPrepare.objects.get(pk__exact=lib_prep)
         lib_prep_obj.set_pool(new_pool)
         # return back to state completed if a library prepatarion was used for reused pool
-        if lib_prep_obj.get_state() == 'Reused pool':
-            lib_prep_obj.set_state('Completed')
+        if lib_prep_obj.get_state() == "Reused pool":
+            lib_prep_obj.set_state("Completed")
 
     # update the number of samples
-    new_pool.set_pool_state('Selected')
+    new_pool.set_pool_state("Selected")
 
     return new_pool
 
 
 def generate_pool_code_id():
-    '''
+    """
     Description:
-        The function get the date of today ( in format yyyy_mm_dd) and check if exists a pool_code_id with the same value.
+        The function get the date of today ( in format yyyy_mm_dd) and check if exists
+        a pool_code_id with the same value.
         If yes the function increment the last digits else is set to "_1"
     Return:
         today_date + subindex
-    '''
+    """
     today_date = date.today().strftime("%Y_%m_%d")
-    if LibraryPool.objects.filter(poolCodeID__icontains = today_date).exists():
-        lib_pool = LibraryPool.objects.filter(poolCodeID__icontains = today_date).last()
+    if LibraryPool.objects.filter(pool_code_id__icontains=today_date).exists():
+        lib_pool = LibraryPool.objects.filter(pool_code_id__icontains=today_date).last()
         latest_code_id = lib_pool.get_pool_code_id()
-        last_seq_number =  int(latest_code_id.split('_')[-1])
-        return str(today_date + '_' + str(last_seq_number + 1))
+        last_seq_number = int(latest_code_id.split("_")[-1])
+        return str(today_date + "_" + str(last_seq_number + 1))
     else:
-        return str(today_date + '_1')
+        return str(today_date + "_1")
 
 
 def get_lib_prep_to_select_in_pool():
-    '''
+    """
     Description:
         The function get the library preparation instances that do not have linked to a pool and
         collect the information to display.
@@ -185,55 +190,65 @@ def get_lib_prep_to_select_in_pool():
         HEADING_FOR_DISPLAY_SAMPLES_IN_POOL
     Return:
         display_list
-    '''
+    """
     display_list = {}
-    display_list['data'] = {}
+    display_list["data"] = {}
 
     from django.db.models import Q
-    if LibPrepare.objects.filter(Q (libPrepState__libPrepState__exact = 'Completed', pools = None )| Q(libPrepState__libPrepState__exact = 'Reused pool' )).exists():
-        lib_preparations =  LibPrepare.objects.filter(Q (libPrepState__libPrepState__exact = 'Completed', pools = None )| Q(libPrepState__libPrepState__exact = 'Reused pool' )).order_by('registerUser')
-        #lib_preparations =  LibraryPreparation.objects.filter(libPrepState__libPrepState = 'Completed', pools = None).order_by('registerUser')
-        for lib_prep in lib_preparations :
+
+    if LibPrepare.objects.filter(
+        Q(lib_prep_state__lib_prep_state__exact="Completed", pools=None)
+        | Q(lib_prep_state__lib_prep_state__exact="Reused pool")
+    ).exists():
+        lib_preparations = LibPrepare.objects.filter(
+            Q(lib_prep_state__lib_prep_state__exact="Completed", pools=None)
+            | Q(lib_prep_state__lib_prep_state__exact="Reused pool")
+        ).order_by("register_user")
+        for lib_prep in lib_preparations:
             user_sample_obj = lib_prep.get_user_sample_sheet_obj()
             platform = user_sample_obj.get_sequencing_configuration_platform()
-            if platform not in display_list['data']:
-                display_list['data'][platform] = []
-            display_list['data'][platform].append( lib_prep.get_info_for_selection_in_pool())
+            if platform not in display_list["data"]:
+                display_list["data"][platform] = []
+            display_list["data"][platform].append(
+                lib_prep.get_info_for_selection_in_pool()
+            )
 
-        display_list['heading'] = wetlab_config.HEADING_FOR_DISPLAY_SAMPLES_IN_POOL
+        display_list["heading"] = wetlab_config.HEADING_FOR_DISPLAY_SAMPLES_IN_POOL
 
     return display_list
 
 
 def get_info_to_display_created_pool(pool_obj):
     information_for_created_pool = {}
-    information_for_created_pool['data'] = pool_obj.get_info()
-    information_for_created_pool['pool_name'] = pool_obj.get_pool_name()
-    information_for_created_pool['heading_pool'] = HEADING_FOR_DISPLAY_CREATED_POOL
+    information_for_created_pool["data"] = pool_obj.get_info()
+    information_for_created_pool["pool_name"] = pool_obj.get_pool_name()
+    information_for_created_pool["heading_pool"] = HEADING_FOR_DISPLAY_CREATED_POOL
     lib_prep_data = []
-    if LibPrepare.objects.filter(pools = pool_obj).exists():
-        lib_prep_ids = LibPrepare.objects.filter(pools = pool_obj)
-        for lib_prep_obj in lib_prep_ids :
+    if LibPrepare.objects.filter(pools=pool_obj).exists():
+        lib_prep_ids = LibPrepare.objects.filter(pools=pool_obj)
+        for lib_prep_obj in lib_prep_ids:
             lib_prep_data.append(lib_prep_obj.get_info_for_display_pool())
-    information_for_created_pool['lib_prep_data'] = lib_prep_data
-    information_for_created_pool['heading_library_pool'] = HEADING_FOR_DISPLAY_LIB_PREP_IN_POOL
+    information_for_created_pool["lib_prep_data"] = lib_prep_data
+    information_for_created_pool[
+        "heading_library_pool"
+    ] = HEADING_FOR_DISPLAY_LIB_PREP_IN_POOL
 
     return information_for_created_pool
 
 
 def get_lib_prep_collection_index(lib_prep_ids):
-    '''
+    """
     Description:
         The function return a list of the used parameters .
     Return:
         protocol_parameter_list.
 
-    '''
+    """
     collection_dict = {}
-    for lib_prep_id in lib_prep_ids :
-        if not LibPrepare.objects.filter(pk__exact = lib_prep_id).exists():
+    for lib_prep_id in lib_prep_ids:
+        if not LibPrepare.objects.filter(pk__exact=lib_prep_id).exists():
             continue
-        lib_prep_obj =  LibPrepare.objects.get(pk__exact = lib_prep_id)
+        lib_prep_obj = LibPrepare.objects.get(pk__exact=lib_prep_id)
         collection_name = lib_prep_obj.get_collection_index_name()
         if collection_name not in collection_dict:
             collection_dict[collection_name] = 0
@@ -243,7 +258,7 @@ def get_lib_prep_collection_index(lib_prep_ids):
     else:
         max_value = 0
         for key, value in collection_dict.items():
-            if value > max_value :
+            if value > max_value:
                 max_value = value
                 collection_name = key
         return collection_name
--- iSkyLIMS_wetlab/utils/testing_wetlab_configuration.py
+++ iSkyLIMS_wetlab/utils/testing_wetlab_configuration.py
@@ -1,95 +1,98 @@
+import grp
 import os
-from django.contrib.auth.models import User
+import pwd
+import shutil
+
 from django.conf import settings
-import pwd, stat, os, grp, shutil
-import logging
 
 from iSkyLIMS_wetlab.models import *
-from iSkyLIMS_wetlab.wetlab_config import *
 from iSkyLIMS_wetlab.utils.common import *
 from iSkyLIMS_wetlab.utils.crontab_process import *
 from iSkyLIMS_wetlab.utils.crontab_update_run_state import *
+from iSkyLIMS_wetlab.wetlab_config import *
 
-def get_config_file (config_file):
+
+def get_config_file(config_file):
     c_file = []
     try:
-        with open (config_file ,'r') as fh:
+        with open(config_file, "r") as fh:
             for line in fh:
-                line = line.replace('\n', '')
+                line = line.replace("\n", "")
                 c_file.append(line)
-    except:
+    except Exception:
         return
     return c_file
 
 
-def get_files_attribute (directory):
+def get_files_attribute(directory):
     attr_files = []
     try:
-        for (dirpath, dirnames, filenames) in os.walk(directory, topdown=True):
+        for dirpath, dirnames, filenames in os.walk(directory, topdown=True):
             for d in dirnames:
-               for entry in os.scandir(directory):
-                    attr_files.append([oct(os.stat(entry).st_mode)[-3:],
-                        pwd.getpwuid(entry.stat().st_uid).pw_name,
-                        grp.getgrgid(entry.stat().st_gid).gr_name,
-                        os.path.join(dirpath, entry.name)])
-    except:
+                for entry in os.scandir(directory):
+                    attr_files.append(
+                        [
+                            oct(os.stat(entry).st_mode)[-3:],
+                            pwd.getpwuid(entry.stat().st_uid).pw_name,
+                            grp.getgrgid(entry.stat().st_gid).gr_name,
+                            os.path.join(dirpath, entry.name),
+                        ]
+                    )
+    except Exception:
         return
     return attr_files
 
 
 def get_iSkyLIMS_settings():
     s_file = []
-    settings_file = os.path.join(settings.BASE_DIR, 'iSkyLIMS' , 'settings.py')
+    settings_file = os.path.join(settings.BASE_DIR, "iSkyLIMS", "settings.py")
     try:
-        with open (settings_file ,'r') as fh:
+        with open(settings_file, "r") as fh:
             for line in fh:
-                line = line.replace('\n', '')
-                if 'PASSWORD' in line or 'SECRET_KEY' in line:
-                    if not 'VALIDATORS' in line:
-                        
-                        split_line = line.split('=')
-                        if len(split_line) >1:
-                            split_line[1] = 'XXXXXXXXXXXXXXXXXX'
-                            line = ' = '.join(split_line)
+                line = line.replace("\n", "")
+                if "PASSWORD" in line or "SECRET_KEY" in line:
+                    if "VALIDATORS" not in line:
+                        split_line = line.split("=")
+                        if len(split_line) > 1:
+                            split_line[1] = "XXXXXXXXXXXXXXXXXX"
+                            line = " = ".join(split_line)
                         else:
-                            split_line = line.split(':')
+                            split_line = line.split(":")
                             if len(split_line) > 1:
-                                split_line[1] = 'XXXXXXXXXXXXXXXXXX'
-                            line = ' : '.join(split_line)
+                                split_line[1] = "XXXXXXXXXXXXXXXXXX"
+                            line = " : ".join(split_line)
                 s_file.append(line)
-    except:
+    except Exception:
         return
 
     return s_file
 
 
-def check_access_database ():
+def check_access_database():
     try:
-        runs = RunProcess.objects.all()
-        return 'OK'
-    except:
-        return 'NOK'
+        RunProcess.objects.all()
+        return "OK"
+    except Exception:
+        return "NOK"
 
 
-def check_samba_connection() :
-
+def check_samba_connection():
     try:
         open_samba_connection()
-        return 'OK'
-    except  :
-        return 'NOK'
+        return "OK"
+    except Exception:
+        return "NOK"
 
 
-def run_exists_in_db (run_name):
-
-    if RunProcess.objects.filter(runName__exact = run_name).exists() :
+def run_exists_in_db(run_name):
+    if RunProcess.objects.filter(run_name__exact=run_name).exists():
         return True
     else:
         return False
 
 
-def folder_test_exists (folder_run_name):
-    '''
+def folder_test_exists(folder_run_name):
+    """
     Description:
         The funtion check if remote folder exists
     Input:
@@ -100,43 +103,52 @@ def folder_test_exists (folder_run_name):
         get_samba_application_shared_folder
     Return:
         True if folder exists
-    '''
+    """
     result = False
     conn = open_samba_connection()
-    run_data_root_folder = os.path.join('/', get_samba_application_shared_folder() )
+    run_data_root_folder = os.path.join("/", get_samba_application_shared_folder())
     shared_folder = get_samba_shared_folder()
-    run_folder_list = conn.listPath( shared_folder, run_data_root_folder)
+    run_folder_list = conn.listPath(shared_folder, run_data_root_folder)
     for sfh in run_folder_list:
         if sfh.isDirectory:
             folder_run = sfh.filename
-            if (folder_run == folder_run_name):
+            if folder_run == folder_run_name:
                 result = True
                 break
     return result
 
 
 def delete_graphic_folder_if_exists(run_name):
-    run_object = RunProcess.objects.get(runName__exact = run_name)
-    if RunningParameters.objects.filter(runName_id = RunProcess.objects.get(runName__exact = run_name)).exists() :
-        folder_graphic = RunningParameters.objects.get(runName_id = RunProcess.objects.get(runName__exact = run_name)).get_run_folder()
-        if os.path.isdir(os.path.join(settings.MEDIA_ROOT,'wetlab', 'images_plot', folder_graphic)) :
-            shutil.rmtree(os.path.join(settings.MEDIA_ROOT,'wetlab', 'images_plot', folder_graphic))
+    if RunningParameters.objects.filter(
+        run_name_id=RunProcess.objects.get(run_name__exact=run_name)
+    ).exists():
+        folder_graphic = RunningParameters.objects.get(
+            run_name_id=RunProcess.objects.get(run_name__exact=run_name)
+        ).get_run_folder()
+        if os.path.isdir(
+            os.path.join(settings.MEDIA_ROOT, "wetlab", "images_plot", folder_graphic)
+        ):
+            shutil.rmtree(
+                os.path.join(
+                    settings.MEDIA_ROOT, "wetlab", "images_plot", folder_graphic
+                )
+            )
     return True
 
 
 def delete_test_run(run_obj):
-    '''
+    """
     Description:
         The funtion delete the database information of the run test
     Input:
         run_obj
-    '''
+    """
     run_obj.delete()
     return
 
 
 def execute_test_for_testing_run(run_test_name, run_test_folder):
-    '''
+    """
     Description:
         The funtion call the functions used in the crontab to collect run information
     Input:
@@ -150,75 +162,82 @@ def execute_test_for_testing_run(run_test_name, run_test_folder):
 
     Return:
         True if folder exists
-    '''
+    """
     run_result = {}
 
-    if RunProcess.objects.filter(runName__exact = run_test_name).exists():
-        run_obj = RunProcess.objects.filter(runName__exact = run_test_name).last()
+    if RunProcess.objects.filter(run_name__exact=run_test_name).exists():
+        run_obj = RunProcess.objects.filter(run_name__exact=run_test_name).last()
         run_obj.delete()
     working_path = settings.MEDIA_ROOT
     os.chdir(working_path)
-    config_file = os.path.join(settings.BASE_DIR,'iSkyLIMS_wetlab',  wetlab_config.LOGGING_CONFIG_FILE )
-    logger=open_log(config_file)
-    logger.info('----------------------------------')
-    logger.info('###########---Start RUN Testing  -----############')
-    logger.info('----------------------------------')
+    config_file = os.path.join(
+        settings.BASE_DIR, "iSkyLIMS_wetlab", wetlab_config.LOGGING_CONFIG_FILE
+    )
+    logger = open_log(config_file)
+    logger.info("----------------------------------")
+    logger.info("###########---Start RUN Testing  -----############")
+    logger.info("----------------------------------")
     search_update_new_runs(run_test_name)
     conn = open_samba_connection()
 
     # Execute 6 times to be sure it has completed all steps
-    state_run_test = ['Sample Sent', 'Processed Run', 'Processed Bcl2fastq']
+    state_run_test = ["Sample Sent", "Processed Run", "Processed Bcl2fastq"]
     for state_run in state_run_test:
-        run_result[state_run] = 'NOK'
-    if not RunProcess.objects.filter(runName__exact = run_test_name).exists():
-        run_result ['ERROR'] = wetlab_config.ERROR_NO_RUN_TEST_WAS_CREATED
+        run_result[state_run] = "NOK"
+    if not RunProcess.objects.filter(run_name__exact=run_test_name).exists():
+        run_result["ERROR"] = wetlab_config.ERROR_NO_RUN_TEST_WAS_CREATED
         return run_result
-    run_obj = RunProcess.objects.filter(runName__exact = run_test_name).last()
+    run_obj = RunProcess.objects.filter(run_name__exact=run_test_name).last()
     for step in range(6):
         state = run_obj.get_state()
-        if state == 'ERROR' :
-            run_result['ERROR'] = 'error'
+        if state == "ERROR":
+            run_result["ERROR"] = "error"
             return run_result
-        elif state == 'Sample Sent':
+        elif state == "Sample Sent":
             manage_run_in_sample_sent_processing_state(conn, [run_obj])
-            if run_obj.get_state() == 'ERROR':
-                run_result ['ERROR'] = 'Error when processing run in Sample Sent state'
+            if run_obj.get_state() == "ERROR":
+                run_result["ERROR"] = "Error when processing run in Sample Sent state"
                 break
             else:
-                run_result['Sample Sent'] = 'OK'
-        elif state == 'Processing Run':
+                run_result["Sample Sent"] = "OK"
+        elif state == "Processing Run":
             manage_run_in_sample_sent_processing_state(conn, [run_obj])
-            if run_obj.get_state() == 'ERROR':
-
-                run_result ['ERROR'] = 'Error when processing run in Processing Run state'
+            if run_obj.get_state() == "ERROR":
+                run_result[
+                    "ERROR"
+                ] = "Error when processing run in Processing Run state"
                 break
             else:
-                run_result['Processing Run'] = 'OK'
-        elif state == 'Processed Run':
+                run_result["Processing Run"] = "OK"
+        elif state == "Processed Run":
             manage_run_in_processed_run_state(conn, [run_obj])
-            if run_obj.get_state() == 'ERROR':
-                run_result ['ERROR'] = 'Error when processing run in Processed Run state'
+            if run_obj.get_state() == "ERROR":
+                run_result["ERROR"] = "Error when processing run in Processed Run state"
                 break
             else:
-                run_result['Processed Run'] = 'OK'
-        elif state == 'Processing Bcl2fastq':
+                run_result["Processed Run"] = "OK"
+        elif state == "Processing Bcl2fastq":
             manage_run_in_processing_bcl2fastq_state(conn, [run_obj])
-            if run_obj.get_state() == 'ERROR':
-                run_result ['ERROR'] = 'Error when processing run in Processing Bcl2fastq state'
+            if run_obj.get_state() == "ERROR":
+                run_result[
+                    "ERROR"
+                ] = "Error when processing run in Processing Bcl2fastq state"
                 break
             else:
-                run_result['Processing Bcl2fastq'] = 'OK'
-        elif state == 'Processed Bcl2fastq':
+                run_result["Processing Bcl2fastq"] = "OK"
+        elif state == "Processed Bcl2fastq":
             manage_run_in_processed_bcl2fastq_state(conn, [run_obj])
-            if run_obj.get_state() == 'ERROR':
-                run_result ['ERROR'] = 'Error when processing run in Processed Bcl2fastq state'
+            if run_obj.get_state() == "ERROR":
+                run_result[
+                    "ERROR"
+                ] = "Error when processing run in Processed Bcl2fastq state"
                 break
             else:
-                run_result['Processed Bcl2fastq'] = 'OK'
-        elif state == 'Completed':
-            run_result['Completed'] = 'OK'
+                run_result["Processed Bcl2fastq"] = "OK"
+        elif state == "Completed":
+            run_result["Completed"] = "OK"
             break
-        logger.info('----------------------------------')
-        logger.info('###########---End RUN Testing  -----############')
-        logger.info('----------------------------------')
+        logger.info("----------------------------------")
+        logger.info("###########---End RUN Testing  -----############")
+        logger.info("----------------------------------")
     return run_result
