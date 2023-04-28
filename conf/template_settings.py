20d048124299a17cf7a0e810b465240342471e26
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-04-28 17:11:12 +0200

    including rename app in install settings, and fixes in templates

--- conf/template_settings.py
+++ conf/template_settings.py
@@ -20,87 +20,88 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = PLACEHOLDER
+SECRET_KEY = SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', 'localserverip']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "localserverip"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'core',
-    'clinic',
-    'wetlab',
-    'drylab',
-    'django_utils',
-    'mptt',
-    'crispy_forms',
-    'crispy_bootstrap3',
-    'django_crontab',
-    'django_mptt_admin',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
-    'rest_framework',
-    'drf_yasg',
-    'django_cleanup', # should go after your apps
+    "core",
+    "clinic",
+    "wetlab",
+    "drylab",
+    "django_utils",
+    "mptt",
+    "crispy_forms",
+    "crispy_bootstrap3",
+    "django_crontab",
+    "django_mptt_admin",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "rest_framework",
+    "drf_yasg",
+    "django_cleanup",
 ]
 
-APPS_NAMES = [ ['clinic', 'Clinic'],
-    ['wetlab', 'Masive Sequencing'],
-    ['drylab','Requesting Services']
-    ]
+APPS_NAMES = [
+    ["clinic", "Clinic"],
+    ["wetlab", "Masive Sequencing"],
+    ["drylab", "Requesting Services"],
+]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'iSkyLIMS.urls'
+ROOT_URLCONF = "iSkyLIMS.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR + '/documents/drylab/services_templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18n',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR + "/documents/drylab/services_templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'iSkyLIMS.wsgi.application'
+WSGI_APPLICATION = "iSkyLIMS.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'USER': 'djangouser',
-        'PASSWORD':'djangopass',
-        'PORT':'djangoport',
-        'NAME': 'djangodbname',
-        'HOST':'djangohost',
-        'TEST': {
-            'NAME': 'iSkyLIMS_test',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "USER": "djangouser",
+        "PASSWORD": "djangopass",
+        "PORT": "djangoport",
+        "NAME": "djangodbname",
+        "HOST": "djangohost",
+        "TEST": {
+            "NAME": "iSkyLIMS_test",
         },
     },
 }
@@ -111,42 +112,34 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-SWAGGER_SETTINGS = {
-    'SECURITY_DEFINITIONS': {
-        'basic': {
-            'type': 'basic'
-        }
-    }
-}
+SWAGGER_SETTINGS = {"SECURITY_DEFINITIONS": {"basic": {"type": "basic"}}}
 
 """ For using token in the authorization request
     'api_key': {
             'type': 'apiKey',
             'in': 'header',
             'name': 'Authorization'
-        }
-     
+        }   
 """
 # 'PERSIST_AUTH': True
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-#TIME_ZONE = 'UTC' --> 'Europe/Madrid' for forms such as 'blog entries'
-TIME_ZONE = 'Europe/Madrid'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "Europe/Madrid"
 
 USE_I18N = True
 
@@ -158,47 +151,42 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 #  Media settings
-MEDIA_URL = '/documents/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'documents/')
+MEDIA_URL = "/documents/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "documents/")
 
-## Crispy forms settings
+#  Crispy forms settings
 CRISPY_TEMPLATE_PACK = "bootstrap3"
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = (
+    "django.core.mail.backends.console.EmailBackend"  # During development only
+)
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # EMAIL settings
-EMAIL_HOST = 'emailhostserver'
-EMAIL_PORT = 'emailport'
-EMAIL_HOST_USER = 'emailhostuser'
-EMAIL_HOST_PASSWORD = 'emailhostpassword'
+EMAIL_HOST = "emailhostserver"
+EMAIL_PORT = "emailport"
+EMAIL_HOST_USER = "emailhostuser"
+EMAIL_HOST_PASSWORD = "emailhostpassword"
 EMAIL_USE_TLS = emailhosttls
 ALLOWED_EMAIL_DOMAINS = []
 
-LOG_CRONTAB_FILE = os.path.join(BASE_DIR, 'logs', 'crontab.log')
-LOG_CLEAN_FILE = os.path.join(BASE_DIR, 'logs', 'crontab_cleanup.log')
+LOG_CRONTAB_FILE = os.path.join(BASE_DIR, "logs", "crontab.log")
+LOG_CLEAN_FILE = os.path.join(BASE_DIR, "logs", "crontab_cleanup.log")
 
 
 # Crontab settings
 CRONJOBS = [
-    #('2-59/5 * * * *', 'wetlab.cron.update_run_in_recorded_state',  '>>' + LOG_CRONTAB_FILE), # run every 5 min wit an offset of 2 minutes
-    #('0 0 2 * *', 'wetlab.cron.update_run_in_recorded_state',  '>>' + LOG_CRONTAB_FILE), # At minute 0 past every 2nd hour
-    #('*/5 * * * *', 'wetlab.cron.check_not_finish_run', '>>' + LOG_CRONTAB_FILE) # run every 5 min
+    ("0 0 2 * *", "wetlab.cron.looking_for_new_runs", ">>" + LOG_CRONTAB_FILE),
+    ("0 0 1 * *", "wetlab.cron.delete_invalid_run", ">>" + LOG_CLEAN_FILE),
+]
 
-    #('0 */2 * * *', 'wetlab.cron.look_for_miseq_runs', '>>' + LOG_CRONTAB_FILE) # At minute 0 past every 2nd hour
-    ('0 0 2 * *', 'wetlab.cron.looking_for_new_runs', '>>' + LOG_CRONTAB_FILE),
-    ('0 0 1 * *', 'wetlab.cron.delete_invalid_run', '>>' + LOG_CLEAN_FILE)
-    ]
+CRONTAB_COMMAND_SUFFIX = "2>&1"
 
-CRONTAB_COMMAND_SUFFIX = '2>&1'
-
-#SITE_ID =1
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
--- conf/urls.py
+++ conf/urls.py
@@ -6,38 +6,48 @@ from drf_yasg import openapi
 
 from drf_yasg.generators import OpenAPISchemaGenerator
 
+
 class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
     def get_schema(self, request=None, public=False):
         schema = super().get_schema(request, public)
         schema.schemes = ["http", "https"]
         return schema
-        
-        
-schema_view = get_schema_view(openapi.Info(
-    title="iSkyLIMS API",
-    default_version='v0.0.1',
-    description="iSkyLIMS API",
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="iSkyLIMS API",
+        default_version="v0.0.1",
+        description="iSkyLIMS API",
     ),
-    generator_class=BothHttpAndHttpsSchemaGenerator, # Here
+    generator_class=BothHttpAndHttpsSchemaGenerator,  # Here
     public=True,
-    
 )
 
 
 urlpatterns = [
-    path('',include('core.urls')),
-    path('background', LoginView.as_view(template_name='core/background.html'), name="background"),
-    path('bu-isciii', LoginView.as_view(template_name='core/bu_isciii.html'), name="about-us"),
-    path('contact', LoginView.as_view(template_name='core/contact.html'), name="contact"),
-    path('admin/', admin.site.urls),
-    path('wetlab/', include('wetlab.urls')),
-    path('drylab/',include('drylab.urls')),
-    path('utils/',include('django_utils.urls')),
-    path('clinic/',include ('clinic.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-    
+    path("", include("core.urls")),
+    path(
+        "background",
+        LoginView.as_view(template_name="core/background.html"),
+        name="background",
+    ),
+    path(
+        "bu-isciii",
+        LoginView.as_view(template_name="core/bu_isciii.html"),
+        name="about-us",
+    ),
+    path(
+        "contact", LoginView.as_view(template_name="core/contact.html"), name="contact"
+    ),
+    path("admin/", admin.site.urls),
+    path("wetlab/", include("wetlab.urls")),
+    path("drylab/", include("drylab.urls")),
+    path("utils/", include("django_utils.urls")),
+    path("clinic/", include("clinic.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     # REST FRAMEWORK URLS
-    path('drylab/api/', include('drylab.api.urls')),
-    path('wetlab/api/', include('wetlab.api.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0))
+    path("drylab/api/", include("drylab.api.urls")),
+    path("wetlab/api/", include("wetlab.api.urls")),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0)),
 ]
