ed80af80c8bd2a9bd7b9cd16c7fae0c40b9c17c8
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-05-09 13:18:32 +0200

    fixed conflicts

--- core/views.py
+++ core/views.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 # from django.shortcuts import render
-import json
-
 from django.core.mail import BadHeaderError, send_mail
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-
 from .forms import ContactForm
+from core.models import Contact
 from .utils.common import *
 
 # from django.conf import settings
@@ -14,25 +11,10 @@ from .utils.common import *
 # Create your views here.
 
 
-=======
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from .forms import ContactForm
-from django.core.mail import send_mail
-
-import core.utils.common 
-import core.models
->>>>>>> Removing the iSkyLIMS_ references in html to statics and organize the statics to clean duplicated/triplicated files
 def index(request):
     apps_in_iskylims = get_installed_apps()
     return render(request, "core/index.html", {"apps_in_iskylims": apps_in_iskylims})
 
-<<<<<<< HEAD
-=======
-    apps_in_iskylims = core.utils.common.get_installed_apps ()
-    return render(request, 'core/index.html',{'apps_in_iskylims': apps_in_iskylims})
->>>>>>> Removing the iSkyLIMS_ references in html to statics and organize the statics to clean duplicated/triplicated files
 
 def add_new_contacts(request):
     """
@@ -46,26 +28,11 @@ def add_new_contacts(request):
 
     Return:
 
-<<<<<<< HEAD
-    """
-
-    """
-    library_kit_objects = BaseSpaceLibraryName.objects.all()
-    if len(library_kit_objects) >0 :
-        for l_kit in library_kit_objects :
-            library_kits.append(l_kit.libraryName)
     """
     apps_installed = {}
     apps_installed["apps_names"] = get_installed_apps()
 
     if request.method == "POST" and request.POST["action"] == "addNewContacts":
-=======
-    '''
-    apps_installed = {}
-    apps_installed['apps_names'] = core.utils.common.get_installed_apps ()
-
-    if request.method == 'POST' and request.POST['action'] == 'addNewContacts':
->>>>>>> Removing the iSkyLIMS_ references in html to statics and organize the statics to clean duplicated/triplicated files
         pass
 
     return render(
@@ -75,7 +42,7 @@ def add_new_contacts(request):
 
 def contact(request):
     contact_info = {}
-    contacts = core.models.Contact.objects.all()
+    contacts = Contact.objects.all()
     for contact in contacts:
         contact_info[contact.get_contact_name()] = contact.get_contact_email()
 
