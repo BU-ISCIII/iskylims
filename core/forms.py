58e21bb2d2c53f35b839ec6d5b415de7a6c8d436
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-05-19 21:34:56 +0200

    fixed contact_email html and css

--- core/forms.py
+++ core/forms.py
@@ -16,43 +16,41 @@ class ContactForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(ContactForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_class = "form-horizontal"
-        self.helper.label_class = "col-lg-5"
-        self.helper.field_class = "col-lg-7"
+        self.helper.form_class = "container-md"
         self.helper.form_action = ""
         self.helper.form_method = "POST"
 
         self.helper.layout = layout.Layout(
             layout.Div(
-                layout.HTML(
-                    """<div class="card-header">
-                        <h3 class="panel-title">Write your email and your request </h3>
-                        </div>"""
-                ),
                 layout.Div(
-                    layout.Div(
-                        layout.Field("email_contact"),
-                        css_class="col-md-10",
-                    ),
-                    layout.Div(
-                        layout.Field("subject"),
-                        css_class="col-md-10",
-                    ),
-                    layout.Div(
-                        layout.Field("message"),
-                        css_class="col-md-10",
-                    ),
-                    layout.Div(
-                        bootstrap.FormActions(
-                            layout.Reset(("Reset"), _("Reset")),
-                            layout.Submit(
-                                ("submit"), _("Submit"), style="margin-left: 80px"
-                            ),
-                        ),
-                        css_class="col-md-10",
-                    ),
-                    css_class="row card-body",
+                    layout.Field("email_contact"),
+                    css_class="col-md-12",
                 ),
-                css_class="card ",
+                css_class="row",
+            ),
+            layout.Div(
+                layout.Div(
+                    layout.Field("subject"),
+                    css_class="col-md-12",
+                ),
+                css_class="row",
+            ),
+            layout.Div(
+                layout.Div(
+                    layout.Field("message"),
+                    css_class="col-md-12",
+                ),
+                css_class="row",
+            ),
+            layout.Div(
+                layout.Div(
+                    bootstrap.FormActions(
+                        layout.Submit(
+                            ("submit"), _("Submit")
+                        ),
+                    ),
+                    css_class="col-md-auto",
+                ),
+                css_class="row justify-content-end"
             ),
         )
