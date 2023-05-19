1955f1a06fa0183baad999ebdb94c1a5d2c7f08e
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-05-19 14:01:13 +0200

    fixing top and style menu

--- core/static/core/main_style/js/main.js
+++ core/static/core/main_style/js/main.js
@@ -1,17 +1,17 @@
-  /**
-   * Scroll top button
-   */
-  const scrollTop = document.querySelector('.scroll-top');
-  if (scrollTop) {
-    const togglescrollTop = function() {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-    }
-    window.addEventListener('load', togglescrollTop);
-    document.addEventListener('scroll', togglescrollTop);
-    scrollTop.addEventListener('click', window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    }));
+/**
+ * Scroll top button
+ */
+const scrollTop = document.querySelector('.scroll-top');
+if (scrollTop) {
+  const togglescrollTop = function() {
+    window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
   }
+  window.addEventListener('load', togglescrollTop);
+  document.addEventListener('scroll', togglescrollTop);
+  scrollTop.addEventListener('click', window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  }));
+}
   
  
