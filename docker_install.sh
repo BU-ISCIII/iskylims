8461f372efaa4dc9da5c7b92b4bab19c5b7ca806
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-06-22 16:37:08 +0200

    fixed samba server so it not uses samba2_10 as min version

--- docker_install.sh
+++ docker_install.sh
@@ -8,14 +8,14 @@ echo "Waiting 20 seconds for starting database and web services..."
 sleep 20
 
 echo "Creating the database structure for iSkyLIMS"
-docker exec -it iskylimsv2_app python3 manage.py migrate
-docker exec -it iskylimsv2_app python3 manage.py makemigrations django_utils core wetlab drylab
-docker exec -it iskylimsv2_app python3 manage.py migrate
+docker exec -it iskylims_app python3 manage.py migrate
+docker exec -it iskylims_app python3 manage.py makemigrations django_utils core wetlab drylab
+docker exec -it iskylims_app python3 manage.py migrate
 
 echo "Loading in database initial data"
-docker exec -it iskylimsv2_app python3 manage.py loaddata conf/first_install_tables.json
+docker exec -it iskylims_app python3 manage.py loaddata conf/first_install_tables.json
 
 echo "Creating super user "
-docker exec -it iskylimsv2_app python3 manage.py createsuperuser
+docker exec -it iskylims_app python3 manage.py createsuperuser
 
 echo "You can now access iskylims via: http://localhost:8001"
