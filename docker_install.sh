3c4661731304e7af7e296a69b402a958dcbd2905
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-07-27 11:28:32 +0200

    docker system working with test data

--- docker_install.sh
+++ docker_install.sh
@@ -112,4 +112,6 @@ fi
 
 echo "Running crontab"
 docker exec -it iskylims_app python3 manage.py crontab add
+docker exec -it iskylims_app cron
+
 echo "You can now access iskylims via: http://localhost:8001"
