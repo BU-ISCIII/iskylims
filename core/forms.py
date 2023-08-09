d8de8c7c185e0b7b6fb125872a64810d4644471c
svarona <sarai.var.fer@gmail.com>
2023-08-09 13:53:10 +0200

    Updated ugettext_lazy to gettext_lazy

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,7 @@
 from crispy_forms import bootstrap, layout
 from crispy_forms.helper import FormHelper
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class ContactForm(forms.Form):
