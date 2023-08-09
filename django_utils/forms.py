a21446430583bad73d2acc74abd1675b4240b278
svarona <sarai.var.fer@gmail.com>
2023-08-09 13:53:10 +0200

    Updated ugettext_lazy to gettext_lazy

--- django_utils/forms.py
+++ django_utils/forms.py
@@ -4,7 +4,7 @@ from crispy_forms.helper import FormHelper
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 # Local imports
 import django_utils.models
