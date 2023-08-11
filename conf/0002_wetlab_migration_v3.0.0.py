58ad9cb321a4055f73f81b62c4e2a778a2e84a65
svarona <sarai.var.fer@gmail.com>
2023-08-11 12:40:06 +0200

    Changed version of core

--- conf/0002_wetlab_migration_v3.0.0.py
+++ conf/0002_wetlab_migration_v3.0.0.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("core", "0002_migration_v2_3_1"),
+        ("core", "0002_migration_v3_0_0"),
         ("wetlab", "0001_initial"),
     ]
 
