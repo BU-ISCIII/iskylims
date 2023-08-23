285e6987af0050e11b591af610bb76dc15f04b19
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-08-23 12:37:28 +0200

    import fixing and some refactor in wetlab api

--- wetlab/api/utils/lab.py
+++ wetlab/api/utils/lab.py
@@ -1,8 +1,8 @@
-from core.models import LabRequest
+import core.models
 
 
 def get_laboratory_instance(lab_name):
     """return the laboratory instance, or None if not match"""
-    if LabRequest.objects.filter(lab_name__iexact=lab_name).exists():
-        return LabRequest.objects.filter(lab_name__iexact=lab_name).last()
+    if core.models.LabRequest.objects.filter(lab_name__iexact=lab_name).exists():
+        return core.models.LabRequest.objects.filter(lab_name__iexact=lab_name).last()
     return None
