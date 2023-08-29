e0c2058ac103b7352c7da4adf7c7d5fa1f4edaca
luissian <chapado.l@gmail.com>
2023-08-29 11:45:13 +0200

    allow in api update lab by using coding name

--- wetlab/api/utils/lab.py
+++ wetlab/api/utils/lab.py
@@ -5,4 +5,10 @@ def get_laboratory_instance(lab_name):
     """return the laboratory instance, or None if not match"""
     if core.models.LabRequest.objects.filter(lab_name__iexact=lab_name).exists():
         return core.models.LabRequest.objects.filter(lab_name__iexact=lab_name).last()
+    elif core.models.LabRequest.objects.filter(
+        lab_name_coding__iexact=lab_name
+    ).exists():
+        return core.models.LabRequest.objects.filter(
+            lab_name_coding__iexact=lab_name
+        ).last()
     return None
