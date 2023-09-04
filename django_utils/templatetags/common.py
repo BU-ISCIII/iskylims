8a31acd101475de844adeae72c8ed629fdf22c50
Sara Monzón <sara.monzon.fdz@gmail.com>
2023-09-04 14:05:17 +0200

    created new template tag to clean strings from weird characters

--- django_utils/templatetags/common.py
+++ django_utils/templatetags/common.py
@@ -1,3 +1,4 @@
+import re
 from django import template
 
 register = template.Library()
@@ -6,3 +7,9 @@ register = template.Library()
 @register.filter
 def get(mapping, key):
     return mapping.get(key, "")
+
+@register.filter(name='clean_string')
+def clean_string(value):
+    # Use regular expression to remove spaces, "-" and "/"
+    cleaned_value = re.sub(r'[-/.,;:\s]', '', value)
+    return cleaned_value
