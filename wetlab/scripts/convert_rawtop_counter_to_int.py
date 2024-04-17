29d4a55f54bd46a15c4e5e5c15f1cf708bd3e27f
luissian <chapado.l@gmail.com>
2024-04-17 23:04:05 +0200

    litin for implemented issue #158 Unable to convert barcode count to integer

--- wetlab/scripts/convert_rawtop_counter_to_int.py
+++ wetlab/scripts/convert_rawtop_counter_to_int.py
@@ -1,9 +1,10 @@
 import wetlab.models
 
+
 def run():
-    """ The script implemted the issue #158 Unable to convert barcode count to
-        integer, to convert the counter that are in a string format, separated
-        by ","  to int.
+    """The script implemted the issue #158 Unable to convert barcode count to
+    integer, to convert the counter that are in a string format, separated
+    by ","  to int.
     """
     top_bar_objs = wetlab.models.RawTopUnknowBarcodes.objects.all()
     for top_bar_obj in top_bar_objs:
