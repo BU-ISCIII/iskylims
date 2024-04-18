cc57dac649e572fae66bfab252d110ace60aa09f
luissian <chapado.l@gmail.com>
2024-04-18 17:14:10 +0200

    Fixing the wetlab part of the issue #264, User can't search service/project by sample name

--- wetlab/views.py
+++ wetlab/views.py
@@ -2884,7 +2884,7 @@ def display_sample_in_run(request, sample_run_id):
     if not wetlab.utils.common.is_wetlab_manager(request):
         # check if user is owner of the run or belongs to the shared user
         shared_user_ids = wetlab.utils.common.get_allowed_user_for_sharing(request.user)
-        if sample_run_obj.get_user_id not in shared_user_ids:
+        if sample_run_obj.get_user_id() not in shared_user_ids:
             return render(
                 request,
                 "wetlab/display_sample.html",
