ad6fc5e0de56724580df6c3b0cd17bcb636ecd40
luissian <chapado.l@gmail.com>
2024-05-17 10:16:20 +0200

    Fixed bug after changed the relation of library_pool-runprocess to many to many

--- wetlab/utils/run.py
+++ wetlab/utils/run.py
@@ -767,8 +767,6 @@ def get_available_pools_for_run():
         pools_to_update
     """
     pools_to_update = {}
-
-    # get the pools that were selected
     if wetlab.models.LibraryPool.objects.filter(
         pool_state__pool_state__exact="Selected"
     ).exists():
