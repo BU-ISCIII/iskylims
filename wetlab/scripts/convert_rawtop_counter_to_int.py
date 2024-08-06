f4f69815603e1d90295d4164de701ac4e67ba233
luissian <chapado.l@gmail.com>
2024-08-06 11:30:04 +0200

    Include condition to reduce updating items to database

--- wetlab/scripts/convert_rawtop_counter_to_int.py
+++ wetlab/scripts/convert_rawtop_counter_to_int.py
@@ -8,9 +8,10 @@ def run():
     """
     top_bar_objs = wetlab.models.RawTopUnknowBarcodes.objects.all()
     for top_bar_obj in top_bar_objs:
-        try:
-            top_bar_obj.count = int(top_bar_obj.count.replace(",", ""))
-            top_bar_obj.save()
-        except AttributeError:
-            continue
+        if "," in top_bar_obj.count:
+            try:
+                top_bar_obj.count = int(top_bar_obj.count.replace(",", ""))
+                top_bar_obj.save()
+            except AttributeError:
+                continue
     return
