f9e920adf4acfd70b3c3bb9d93a5114b6fc2fa13
Sara Monzón <sara.monzon.fdz@gmail.com>
2025-03-18 17:42:21 +0100

    manage error email send for delivery notification

--- drylab/utils/deliveries.py
+++ drylab/utils/deliveries.py
@@ -1,5 +1,6 @@
 # Generic imports
 import datetime
+from smtplib import SMTPException
 
 import django.core.mail
 
@@ -133,6 +134,6 @@ def send_delivery_service_email(email_data):
     to_users = [email_data["user_email"], email_data["user_email"], notification_user]
     try:
         django.core.mail.send_mail(subject, body_message, from_user, to_users)
-    except Exception:
-        pass
+    except (SMTPException, ConnectionRefusedError):
+        raise
     return
