d70e33b91a52c682e05fe4e6f9bbddd53e9dde4b
Shettland <pyblus@gmail.com>
2025-04-08 13:45:31 +0200

    Included reviewer changes

--- docker_install.sh
+++ docker_install.sh
@@ -92,7 +92,7 @@ done
 shift $((OPTIND-1))
 
 echo "Deploying test containers with INSTALL_TYPE=$install_type and GIT_REVISION=$git_revision..."
-docker compose build --build-arg INSTALL_TYPE=$install_type --build-arg GIT_REVISION=$git_revision
+docker compose build --no-cache --build-arg INSTALL_TYPE=$install_type --build-arg GIT_REVISION=$git_revision
 docker compose up -d
 
 echo "Waiting 20 seconds for starting database and web services..."
